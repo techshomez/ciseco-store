7fefa27f8aae0136fa6f13b139d8ff3119dbc971
techshomez <danieltopeg@gmail.com>
2023-08-25 17:16:10 +0100

    navbar mobile responsive

--- src/components/common/navbar/index.jsx
+++ src/components/common/navbar/index.jsx
@@ -1,42 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import { FiSearch, FiUser, FiShoppingCart } from "react-icons/fi";
+import { FiMenu, FiSearch, FiUser, FiX } from "react-icons/fi";
+import { BsCart2, BsChevronDown } from "react-icons/bs";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState("navbar");
+
+  const handleMenu = () => {
+    setOpenMenu("mobile");
+  };
+
+  const handleCloseMenu = () => {
+    setOpenMenu("navbar");
+  };
+
   return (
     <header className="header">
-      <nav className="navbar">
-        <div className="logo">
-          <Link to="/">
-            <img src="/images/logo.svg" alt="logo" />
-          </Link>
+      <div className="logo">
+        <Link to="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </Link>
+      </div>
+      <nav className={openMenu}>
+        <Link to="">men</Link>
+        <Link to="">women</Link>
+        <Link to="">beauty</Link>
+        <Link to="">sport</Link>
+        <div to="" className="has_sub">
+          templates
+          <i>
+            <BsChevronDown />
+          </i>
         </div>
-        <div className="nav__link">
-          <Link to="">men</Link>
-          <Link to="">women</Link>
-          <Link to="">beauty</Link>
-          <Link to="">sport</Link>
-          <Link to="">templates</Link>
-          <Link to="">explore</Link>
-        </div>
-        <div className="nav__icons">
-          <span>
-            <i>
-              <FiSearch />
-            </i>
-          </span>
-          <span>
-            <i>
-              <FiUser />
-            </i>
-          </span>
-          <span>
-            <i>
-              <FiShoppingCart />
-            </i>
-          </span>
+        <div to="" className="has_sub">
+          explore
+          <i>
+            <BsChevronDown />
+          </i>
         </div>
       </nav>
+      <div className="nav__icons">
+        <span>
+          <i>
+            <FiSearch />
+          </i>
+        </span>
+        <span>
+          <i>
+            <FiUser />
+          </i>
+        </span>
+        <span>
+          <i>
+            <BsCart2 />
+          </i>
+          <small>3</small>
+        </span>
+      </div>
+      <div className="hambuger" onClick={handleMenu}>
+        <i>
+          <FiMenu />
+        </i>
+      </div>
     </header>
   );
 };
