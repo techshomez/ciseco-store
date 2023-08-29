7e0f9aaa9832081fae00cad11cc9f3168e50295b
techshomez <danieltopeg@gmail.com>
2023-08-29 13:18:36 +0100

    Discover component;

--- src/components/home/discover/index.jsx
+++ src/components/home/discover/index.jsx
@@ -30,13 +30,46 @@ const Discover = () => {
       <div className="discover__wrapper">
         <div className="discover__item">
           <Carousel responsive={responsive}>
-            <h1>hi</h1>
-            <h1>hi</h1>
-            <h1>hi</h1>
-            <h1>hi</h1>
-            <h1>hi</h1>
-            <h1>hi</h1>
-            <h1>hi</h1>
+            <div className="discover__item__list">
+              <div className="item__list__left">
+                <h4>Explore new arrivals</h4>
+                <h1>Shop the latest from top brands</h1>
+                <utton>Show me all</utton>
+              </div>
+              <div className="item__list__right">
+                <img src="/images/discover/2.png" alt="" />
+              </div>
+            </div>
+            <div className="discover__item__list">
+              <div className="item__list__left">
+                <h4>Explore new arrivals</h4>
+                <h1>Shop the latest from top brands</h1>
+                <utton>Show me all</utton>
+              </div>
+              <div className="item__list__right">
+                <img src="/images/discover/2.png" alt="" />
+              </div>
+            </div>
+            <div className="discover__item__list">
+              <div className="item__list__left">
+                <h4>Explore new arrivals</h4>
+                <h1>Shop the latest from top brands</h1>
+                <utton>Show me all</utton>
+              </div>
+              <div className="item__list__right">
+                <img src="/images/discover/2.png" alt="" />
+              </div>
+            </div>
+            <div className="discover__item__list">
+              <div className="item__list__left">
+                <h4>Explore new arrivals</h4>
+                <h1>Shop the latest from top brands</h1>
+                <utton>Show me all</utton>
+              </div>
+              <div className="item__list__right">
+                <img src="/images/discover/2.png" alt="" />
+              </div>
+            </div>
           </Carousel>
         </div>
       </div>
