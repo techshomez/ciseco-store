fdf8c7ef7c80400b418707e0653775344033dcee
techshomez <danieltopeg@gmail.com>
2023-09-04 21:36:51 +0100

    styling earn component

---
+++ src/components/reuse/button/medium/large/default/index.jsx
@@ -0,0 +1,7 @@
+import "./default.css";
+
+const DefaultButton = ({ value }) => {
+  return <button className="default">{value}</button>;
+};
+
+export default DefaultButton;
--- src/components/reuse/earnCiseco/index.jsx
+++ src/components/reuse/earnCiseco/index.jsx
@@ -1,7 +1,27 @@
 import "./earn.css";
+import LargeButton from "../button/large/darkBlue";
+import DefaultButton from "../button/large/default";
 
 const Earn = () => {
-  return <div>Earn</div>;
+  return (
+    <div className="earn">
+      <div className="earn__left">
+        <img src="/images/logo.svg" alt="" />
+        <h1>
+          Earn free money <br />
+          with Ciseco
+        </h1>
+        <article>With Ciseco you will get freeship & savings combo...</article>
+        <div className="earn__cta">
+          <LargeButton value="Savings combo" />
+          <DefaultButton value="Discover more" />
+        </div>
+      </div>
+      <div className="earn__right">
+        <img src="/images/rightLargeImg.webp" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Earn;
