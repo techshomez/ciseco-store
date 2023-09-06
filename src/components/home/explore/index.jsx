3b534ddf95fd4c38b6667e950e9e611abd56e23b
techshomez <danieltopeg@gmail.com>
2023-09-06 20:41:47 +0100

    explore component styling done

--- src/components/home/explore/index.jsx
+++ src/components/home/explore/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./explore.css";
-import { BsArrowRightShort } from "react-icons/bs";
+import { BsArrowRight } from "react-icons/bs";
 const Explore = () => {
   return (
     <div className="explore">
@@ -11,31 +11,24 @@ const Explore = () => {
             <img src="/images/product/7.png" alt="" />
             <span>155 products</span>
           </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
-          </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
-          </div>
-        </div>
-        <div className="explore__item">
-          <div className="item__top">
-            <img src="/images/product/7.png" alt="" />
-            <span>155 products</span>
-          </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
-          </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">
+                  see collection
+                  <i>
+                    <BsArrowRight />
+                  </i>
+                </Link>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore1.svg" alt="" />
+            </div>
           </div>
         </div>
         <div className="explore__item">
@@ -43,31 +36,22 @@ const Explore = () => {
             <img src="/images/product/7.png" alt="" />
             <span>155 products</span>
           </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
-          </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
-          </div>
-        </div>
-        <div className="explore__item">
-          <div className="item__top">
-            <img src="/images/product/7.png" alt="" />
-            <span>155 products</span>
-          </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
-          </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">see collection</Link>
+                <i>
+                  <BsArrowRight />
+                </i>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore2.svg" alt="" />
+            </div>
           </div>
         </div>
         <div className="explore__item">
@@ -75,15 +59,22 @@ const Explore = () => {
             <img src="/images/product/7.png" alt="" />
             <span>155 products</span>
           </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
-          </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">see collection</Link>
+                <i>
+                  <BsArrowRight />
+                </i>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore3.svg" alt="" />
+            </div>
           </div>
         </div>
         <div className="explore__item">
@@ -91,15 +82,68 @@ const Explore = () => {
             <img src="/images/product/7.png" alt="" />
             <span>155 products</span>
           </div>
-          <div className="item__middle">
-            <h4>Manufacturer</h4>
-            <span>Backpack</span>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">see collection</Link>
+                <i>
+                  <BsArrowRight />
+                </i>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore4.svg" alt="" />
+            </div>
           </div>
-          <div className="item_bottom">
-            <Link to="">see collection</Link>
-            <i>
-              <BsArrowRightShort />
-            </i>
+        </div>
+        <div className="explore__item">
+          <div className="item__top">
+            <img src="/images/product/7.png" alt="" />
+            <span>155 products</span>
+          </div>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">see collection</Link>
+                <i>
+                  <BsArrowRight />
+                </i>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore5.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="explore__item">
+          <div className="item__top">
+            <img src="/images/product/7.png" alt="" />
+            <span>155 products</span>
+          </div>
+          <div className="item__content">
+            <div className="item__left">
+              <div className="item__left__content">
+                <span>Manufacturer</span>
+                <h1>Backpack</h1>
+              </div>
+              <div className="item__left__cta">
+                <Link to="">see collection</Link>
+                <i>
+                  <BsArrowRight />
+                </i>
+              </div>
+            </div>
+            <div className="item__right">
+              <img src="/images/explore6.svg" alt="" />
+            </div>
           </div>
         </div>
       </div>
