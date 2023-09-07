49a969201009390c6b870f414d4451e24436f83a
techshomez <danieltopeg@gmail.com>
2023-09-07 11:44:32 +0100

    Adding explore tab component responsiveness

--- src/components/home/explore/tab/index.jsx
+++ src/components/home/explore/tab/index.jsx
@@ -1,46 +1,70 @@
 import "./tab.css";
-import { BsGenderFemale } from "react-icons/bs";
+import {
+  BsGenderFemale,
+  BsGenderMale,
+  BsPerson,
+  BsArrowLeftRight,
+  BsSuitDiamond,
+  BsXDiamond,
+} from "react-icons/bs";
+
+const tab_item = [
+  {
+    id: 1,
+    title: "Woman",
+    icon: <BsGenderFemale />,
+    exploreID: 1,
+    cl: "tab__active",
+  },
+  {
+    id: 2,
+    title: "Man",
+    icon: <BsGenderMale />,
+    exploreID: 2,
+  },
+  {
+    id: 3,
+    title: "Kids",
+    icon: <BsPerson />,
+    exploreID: 1,
+  },
+  {
+    id: 4,
+    title: "Sports",
+    icon: <BsArrowLeftRight />,
+    exploreID: 1,
+  },
+  {
+    id: 5,
+    title: "Beauty",
+    icon: <BsSuitDiamond />,
+    exploreID: 1,
+  },
+  {
+    id: 6,
+    title: "Jewelry",
+    icon: <BsXDiamond />,
+    exploreID: 1,
+  },
+];
 
 const ExploreTab = () => {
   return (
     <div className="tab">
       <div className="tab__item">
-        <div className="tab__item__list">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
-        <div className="tab__item__list">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
-        <div className="tab__item__list tab__active">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
-        <div className="tab__item__list">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
-        <div className="tab__item__list">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
-        <div className="tab__item__list">
-          <i>
-            <BsGenderFemale />
-          </i>
-          <span>Woman</span>
-        </div>
+        {tab_item.map((list) => {
+          return (
+            <div
+              className={
+                list.cl ? "tab__item__list tab__active" : "tab__item__list"
+              }
+              key={list.id}
+            >
+              <i>{list.icon}</i>
+              <span>{list.title}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
